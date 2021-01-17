3b2e956052b8b65e7b1ce3d6eac21cb9c7ef9d5d
RDGrover <rossgrover95@hotmail.co.uk>
2021-01-17 13:02:10 +0000

    create home page and connect to mongo db

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -9,9 +11,18 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/movie_index")
+def movie_index():
+    movies = mongo.db.movies.find()
+    return render_template("movies.html", movies=movies)
 
 
 if __name__ == "__main__":
