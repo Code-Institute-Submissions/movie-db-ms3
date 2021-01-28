ef912463296861f4c23d2087e1b439a4b3af78d5
RDGrover <rossgrover95@hotmail.co.uk>
2021-01-28 21:09:43 +0000

    create about page and add basic structure

--- app.py
+++ app.py
@@ -97,6 +97,11 @@ def add_review():
     return render_template("add_review.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
