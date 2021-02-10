a8deb7ea696be26d0cc1360dddec9e7b06e462ea
RDGrover <rossgrover95@hotmail.co.uk>
2021-02-10 00:42:48 +0000

    add semicolon

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     let sidenavs = document.querySelectorAll(".sidenav");
     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
-})
+});
 function remove(){
     if(confirm("Are you sure you want to delete your review?")){
         delete_one();
