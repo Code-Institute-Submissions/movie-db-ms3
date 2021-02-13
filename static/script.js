d190cb5a143d197638294662dd3dbaa6660623d2
RDGrover <rossgrover95@hotmail.co.uk>
2021-02-13 16:43:51 +0000

    add comments to javascript and css

--- static/script.js
+++ static/script.js
@@ -1,7 +1,9 @@
+// Creates hamburger menu for mobile 
 document.addEventListener('DOMContentLoaded', function () {
     let sidenavs = document.querySelectorAll(".sidenav");
     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
 });
+// Asks for confirmation when you press delete button on reviews
 function remove(){
     if(confirm("Are you sure you want to delete your review?")){
         delete_one();
