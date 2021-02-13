ee94bc7254e3121a90033091ec89ca79df32c542
RDGrover <rossgrover95@hotmail.co.uk>
2021-02-13 16:50:24 +0000

    set debug to false

--- app.py
+++ app.py
@@ -163,4 +163,4 @@ def delete_review(movie_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
